fe162db39b42fe9407d294562e2dd2692b4ee28c
Ayyoob Ajward <ajwardayyoob@gmail.com>
2024-11-10 23:09:23 +0530

    Add error handling

--- src/functions/city-wheather/handler.ts
+++ src/functions/city-wheather/handler.ts
@@ -1,5 +1,5 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
-import { successResponse } from '@libs/api-gateway';
+import { clientErrorResponse, successResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 import axios from 'axios';
 require('dotenv').config();
@@ -11,11 +11,29 @@ const city_wheather: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
 
   const { city } = event.body;
 
-  const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${OPENWEATHERMAP_API_KEY}`);
-  
-  return successResponse({
-    wheather: response.data,
-  });
+  if (!city) {
+    return clientErrorResponse({
+      Error: "Please enter a city"
+    });
+  }
+
+  try {
+    const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${OPENWEATHERMAP_API_KEY}`);
+
+    if (response.data.length === 0) {
+      return clientErrorResponse({
+        Error: `No weather data found for city: ${city}`
+      });
+    }
+
+    return successResponse({
+      wheather: response.data,
+    });
+  } catch (error) {
+    return clientErrorResponse({
+      Error: `Unexpected error occurred - ${error.message}`,
+    });
+  }
 };
 
 export const main = middyfy(city_wheather);
